e6179b607d28c3429f787daaaa1a46b1441d17c4
Yaron Horsky <horsky.yaron@gmail.com>
2020-12-02 01:22:41 +0200

    started working on copy function. made a function for checking if file had a .txt suffix

--- ex2.c
+++ ex2.c
@@ -1,19 +1,77 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+
+const int SRC_ARG = 1;
+const int DST_ARG = 2;
+const int SRC_OS_ARG = 3;
+const int DST_OS_ARG = 4;
+const int ENDIANNESS_ARG = 5;
+const int MIN_FILE_NAME_SIZE = 4;
+
+bool isTextFile(char* argument) {
+    //check if a given argument is a text file.
+    int argLength = strlen(argument);
+
+    //minimum length for file name is 4 ("X.txt").
+    if (argLength >= MIN_FILE_NAME_SIZE) {
+
+        //slicing the argument string and getting the last four chars.
+        char *argSuffix = argument + (argLength - 4) * sizeof(char);
+
+        const char *txtSuffix = ".txt";
+
+        //checks if the end of the file argument has ".txt" in the end.
+        return (strcmp(argSuffix, txtSuffix) == 0);
+    } else {
+        return false;
+    }
+}
+
+
+//this function will create a copy of a given txt file.
+void copy(char *srcFile, char *dstFile) {
+    if(isTextFile(srcFile) && isTextFile(dstFile)) {
+        FILE *src;
+        FILE *dst;
+
+        src = fopen(srcFile, "rb");
+        if(src != NULL) {
+             n
+        }
+    }
+
+}
+
+void osSensitiveCopy(char *srcFile, char *dstFile, char *srcOs, char *dstOs, char *endianness) {
+    if(endianness != NULL) {
+        printf("5 arguments scenario");
+    } else {
+        printf("4 arguments scenario");
+    }
+}
 
 int main(int argc, char *argv[]) {
-    switch(argc-1) {
-        case 2:
-            printf("you entered 1 argument\n");
+
+    int numOfArguments = argc - 1;
+
+    switch(numOfArguments) {
+        case 2: {
+            copy(argv[SRC_ARG], argv[DST_ARG]);
             break;
-        case 4:
-            printf("you entered 2 argument\n");
+        }
+        case 4: {
+            osSensitiveCopy(argv[SRC_ARG], argv[DST_ARG], argv[SRC_OS_ARG], argv[DST_OS_ARG], NULL);
             break;
-        case 5:
-            printf("you entered 1 argument\n");
+        }
+        case 5: {
+            osSensitiveCopy(argv[SRC_ARG], argv[DST_ARG], argv[SRC_OS_ARG], argv[DST_OS_ARG], argv[ENDIANNESS_ARG]);
             break;
-        default:
-            printf("Not a valid ");
+        }
+        default: {
+            printf("Please enter a correct number of arguments.\n");
+        }
     }
 
     return 0;
